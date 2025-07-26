0c98da4ecd3f71d57f693789dc4d85aac7670147
flames <isaacomenuche@gmail.com>
2025-07-26 05:16:53 +0100

    feat: refactor ChatSocketProvider to handle user ID fetching and loading state

--- src/app/(games)/layout.tsx
+++ src/app/(games)/layout.tsx
@@ -1,24 +1,16 @@
 import Chat from "@/components/games/chat";
-import RequireAuth from "@/components/require-auth";
 import { ChatSocketProvider } from "@/contexts/ChatSocketProvider";
 import { ConnectUserProvider } from "@/contexts/ConnectWalletContext";
-import { getClaimFromJwt } from "@/lib/getClaimFromJwt";
 
 interface LayoutProps {
 	children: Readonly<React.ReactNode>;
 }
 
-export default async function Layout({ children }: LayoutProps) {
-	const userId = await getClaimFromJwt<string>("sub");
-
-	if (!userId) {
-		return <RequireAuth />;
-	}
-
+export default function Layout({ children }: LayoutProps) {
 	return (
 		<ConnectUserProvider>
 			<main className="flex-1 bg-primary/10">
-				<ChatSocketProvider userId={userId}>
+				<ChatSocketProvider>
 					{children}
 					<Chat />
 				</ChatSocketProvider>
