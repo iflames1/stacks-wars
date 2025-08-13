0cc358e5e02c0bd1125f523f0330267e990a0162
Flames <isaacomenuche@gmail.com>
2025-08-13 20:26:04 +0100

    feat: enhance lobby stats and participants components to handle null entry amounts and adjust calculations accordingly

--- src/lib/actions/joinGamePool.ts
+++ src/lib/actions/joinGamePool.ts
@@ -15,7 +15,7 @@ export const joinGamePool = async (
 			type: "stx-postcondition",
 			address,
 			condition: "eq",
-			amount: amount * 1000000,
+			amount: amount * 1_000_000,
 		};
 
 		return await request("stx_callContract", {
@@ -25,6 +25,44 @@ export const joinGamePool = async (
 			network: "testnet",
 			postConditionMode: "deny",
 			postConditions: [stxPostCondition],
+		});
+	} catch (error) {
+		console.error("Wallet returned an error:", error);
+		throw error;
+	}
+};
+
+export const joinSponsoredGamePool = async (
+	contract: `${string}.${string}`,
+	deployerAddress: string,
+	amount: number
+) => {
+	const address = await getClaimFromJwt<string>("wallet");
+	if (!address) {
+		throw new Error("No wallet address found");
+	}
+	try {
+		let postConditions: StxPostCondition[] = [];
+
+		if (deployerAddress === address) {
+			const stxPostCondition: StxPostCondition = {
+				type: "stx-postcondition",
+				address,
+				condition: "eq",
+				amount: amount * 1_000_000,
+			};
+			postConditions = [stxPostCondition];
+		}
+
+		console.log(contract, postConditions);
+
+		return await request("stx_callContract", {
+			contract,
+			functionName: "join-pool",
+			functionArgs: [],
+			network: "testnet",
+			postConditionMode: "deny",
+			postConditions,
 		});
 	} catch (error) {
 		console.error("Wallet returned an error:", error);
