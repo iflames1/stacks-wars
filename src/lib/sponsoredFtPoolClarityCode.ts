5f15b5c277215e5220d9ccd2512508216a7d08fc
flames <isaacomenuche@gmail.com>
2025-08-19 12:12:15 +0100

    feat: refactor leave-pool function to improve clarity and streamline token withdrawal process

--- src/lib/sponsoredFtPoolClarityCode.ts
+++ src/lib/sponsoredFtPoolClarityCode.ts
@@ -110,40 +110,6 @@ export const getSponsoredFtClarityCode = (
     )
 )
 
-(define-public (leave-pool)
-    (begin
-        ;; Ensure player has joined
-        (let ((player-data (unwrap! (map-get? players {player: tx-sender}) (err ERR_NOT_JOINED))))
-            (if (get is-sponsor player-data)
-                ;; Sponsor leaving - withdraw remaining tokens
-                (begin
-                    ;; Ensure no other players are still in the pool
-                    (asserts! (is-eq (var-get total-players) u1) (err ERR_POOL_NOT_EMPTY))
-
-                    (let ((balance (unwrap-panic (contract-call? ${tokenContract} get-balance (as-contract tx-sender)))))
-                        (match (as-contract (contract-call? ${tokenContract} transfer balance tx-sender DEPLOYER none))
-                            success
-                            (begin
-                                (map-delete players {player: tx-sender})
-                                (var-set total-players (- (var-get total-players) u1))
-                                (var-set pool-funded false)
-                                (ok true)
-                            )
-                            error (err ERR_TRANSFER_FAILED)
-                        )
-                    )
-                )
-                ;; Regular player leaving
-                (begin
-                    (map-delete players {player: tx-sender})
-                    (var-set total-players (- (var-get total-players) u1))
-                    (ok true)
-                )
-            )
-        )
-    )
-)
-
 (define-public (leave-pool (signature (buff 65)))
     (begin
         ;; Ensure player has joined
@@ -183,6 +149,56 @@ export const getSponsoredFtClarityCode = (
                         (var-set total-players (- (var-get total-players) u1))
                         (ok true)
                     )
+                )
+            )
+        )
+    )
+)
+
+
+(define-public (claim-reward (amount uint) (signature (buff 65)))
+    (begin
+        (asserts! (is-some (map-get? players {player: tx-sender})) (err ERR_NOT_JOINED))
+        (asserts! (not (is-some (map-get? claimed-rewards {player: tx-sender}))) (err ERR_REWARD_ALREADY_CLAIMED))
+
+        (let (
+            (msg-hash (try! (construct-message-hash amount)))
+            (recipient tx-sender)
+            (fee (/ (* amount FEE_PERCENTAGE) u100))
+            (net-amount (- amount fee))
+            (has-paid-fee (has-paid-entry-fee tx-sender))
+            (current-balance (unwrap-panic (contract-call? ${tokenContract} get-balance (as-contract tx-sender))))
+        )
+            (asserts! (secp256k1-verify msg-hash signature TRUSTED_PUBLIC_KEY) (err ERR_INVALID_SIGNATURE))
+            (asserts! (>= current-balance amount) (err ERR_INSUFFICIENT_FUNDS))
+
+            ;; Handle the fee payment conditionally
+            (let ((fee-result
+                (if (not has-paid-fee)
+                    ;; Transfer fee if not already paid
+                    (match (as-contract (contract-call? ${tokenContract} transfer fee tx-sender STACKS_WARS_FEE_WALLET none))
+                        fee-success
+                        (begin
+                            ;; Mark fee as collected
+                            (map-set collected-fees {player: tx-sender} {paid: true})
+                            (ok true)
+                        )
+                        fee-error (err ERR_FEE_TRANSFER_FAILED)
+                    )
+                    (ok true)  ;; Skip fee if already paid
+                )))
+
+                ;; Check if fee payment was successful
+                (try! fee-result)
+
+                ;; Transfer reward to player
+                (match (as-contract (contract-call? ${tokenContract} transfer net-amount tx-sender recipient none))
+                    reward-success
+                    (begin
+                        (map-set claimed-rewards {player: recipient} {claimed: true, amount: amount})
+                        (ok true)
+                    )
+                    reward-error (err ERR_TRANSFER_FAILED)
                 )
             )
         )
